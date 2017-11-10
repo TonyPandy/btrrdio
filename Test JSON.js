54a222289a2c5b58d8614e89f2036c0a47f78286
Sam <33269476+GarchDev@users.noreply.github.com>
2017-11-10 13:17:34 +1030

    added CORS-anywhere URL prepended to xml request

--- Test JSON.js
+++ Test JSON.js
@@ -1,4 +1,3 @@
-
 function getJSONPods (url) {
     var Httpreq = new XMLHttpRequest();
     Httpreq.open("GET",url,false);
@@ -8,14 +7,43 @@ function getJSONPods (url) {
 
 function renameThisFunction() {
     var searchTerm = document.getElementById("searchField").value;
-    var url = 'https://itunes.apple.com/search?term=' + searchTerm + '&limit=200&media=podcast';
+    //var url = 'https://itunes.apple.com/search?term=' + searchTerm + '&limit=7&media=podcast';
+    var url = 'https://itunes.apple.com/search?term=' + searchTerm + '&media=podcast';
     var resultsDiv = document.getElementById("resultsDiv");
-    var json_obj = JSON.parse(getJSONPods(url))
+    var json_obj = JSON.parse(getJSONPods(url));
     resultsDiv.innerHTML = "";
-    for (var i = 0; i < json_obj.resultCount; i++) {
-        //alert(json_obj.results[i].artistName);
+
+    if (json_obj.resultCount == 0) {
+        resultsDiv.innerHTML = "<br>No results found!";
+    }
+    
+    for (var i = 0; i < json_obj.resultCount; i++) { //loop for the podcast results
+        //alert(json_obj.results[i].feedUrl);
+
         resultsDiv.innerHTML += "name: " + json_obj.results[i].artistName +
-            "<br><img name='test" + i + "' src=" + json_obj.results[i].artworkUrl100 + "><br>";
+            "<br><img name='test" + i + "' src=" + json_obj.results[i].artworkUrl100 + "><br>"; //output title + image
+
+        getXML('https://cors-anywhere.herokuapp.com/'+json_obj.results[i].feedUrl);
     }
 }
 
+function getXML (url) {
+    $.get(url, function (data) {
+        console.log(data);
+        $(data).find("channel > title").each(function () { // or "item" or whatever suits your feed
+            var el = $(this);
+            console.log("===Podcast Info===");
+            console.log(el["0"].textContent);
+
+
+            console.log("===Episodes from this podcast===");
+            $(data).find("item").each(function () {
+                var itemEl = $(this);
+
+                console.log("title      : " + itemEl.find("title").text());
+                console.log("description: " + itemEl.find("description").text());
+                console.log(itemEl.find("enclosure")["0"].attributes[2].nodeValue);
+            });
+        });
+    });
+}
