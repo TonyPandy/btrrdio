7fc3470b2d55cf2a0ad5a5185e37afb36c7f7064
Aaron Shannon-Honson <aaron.shannon-honson@adelaide.edu.au>
2017-12-21 15:34:00 +1030

    Search results fixed, new results page added.
    Still need to add to the results layout. Make it look less shit, and add in something to continue to load older episodes past the 20th result

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ function getJSONPods (url) {
 function findPod() {
     var resultsDiv = document.getElementById("resultsDiv");
     var searchTerm = document.getElementById("searchField").value;
-    var url = 'https://itunes.apple.com/search?term=' + searchTerm + '&media=podcast&limit=6';
+    var url = 'https://itunes.apple.com/search?term=' + searchTerm + '&media=podcast&limit=10';
     //https://itunes.apple.com/au/rss/topaudiopodcasts/limit=20/json - example request to return top podcasts
 
     //clear search results
@@ -31,11 +31,6 @@ function findPod() {
 
     for (var i = 0; i < resultCount; i++) {
         outputResults(json_obj.results[i]);
-        //NOTE: this could probably be streamlined combining outputResults adn getXML into one method or something, it's a little messy right now but it's fine.
-
-        //NOTE: changes made: outputResults gets called with the results, that then adds the name of the podcast, the artist behind the podcast, the icon and blank spans/p tags with unique ids that get filled in with episode information.
-        //Then, getXML gets called with the XML parameters (cors-anywhere shit) as well as the trackID from the JSON. This is what joins the XML and JSON results for display
-        //Then in getXML, the same stuff happens, I just added in a bit where the description for the first episode (latest episode) also gets returned.
     }
 
     return false;
@@ -45,9 +40,9 @@ function outputResults(jsonResults) {
     var resultsOutput;
     var resultsDiv = document.getElementById("resultsDiv");
     console.log(jsonResults);
-    var getXMLParameters = jsonResults.feedUrl;
-// 'https://cors-anywhere.herokuapp.com/' +
-  resultsOutput = "<div class='row podResult'><div class='col-sm-3'><img src='" + jsonResults.artworkUrl600 + "' style='width: 100%;'></div> <div class='col-sm-9'><a href='#' class='podTitle' id='result" + jsonResults.trackId + "'>" + jsonResults.trackName + "</a><br><p class='artist'>" + jsonResults.artistName + "</p><p id='description" + jsonResults.trackId + "'></p><br><span id='epList" + jsonResults.trackId + "'>See Episode List</span><br>Latest Ep: <strong><span id='epTitle" + jsonResults.trackId + "'></span></strong><br><span id=playEp" + jsonResults.trackId +"> </span><br><span id='epListing" + jsonResults.trackId + "'></span></div></div></div>";
+    var getXMLParameters = "https://cors-anywhere.herokuapp.com/" + jsonResults.feedUrl;
+// '
+  resultsOutput = "<div class='row podResult'><div class='col-sm-3'><img src='" + jsonResults.artworkUrl600 + "' style='width: 100%;'></div> <div class='col-sm-9'><a href='results.html?result=" + jsonResults.trackId + "' class='podTitle' id='result" + jsonResults.trackId + "'>" + jsonResults.trackName + "</a><br><p class='artist'>" + jsonResults.artistName + "</p><p id='description" + jsonResults.trackId + "'></p><br><span id='epList" + jsonResults.trackId + "'></span><br>Latest Ep: <strong><span id='epTitle" + jsonResults.trackId + "'></span></strong><br><span id=playEp" + jsonResults.trackId +"> </span><br><span id='epListing" + jsonResults.trackId + "' class='episodeListings'></span></div></div></div>";
 // <p id='epDescript" + jsonResults.trackId + "'></p>
       //getXML is now called for each outputResults
       getXML(getXMLParameters, jsonResults.trackId, resultsOutput);
@@ -71,20 +66,57 @@ function getXML (url, jsonTrackId, resultsHTML) {
           //document.getElementById("epDescript"+jsonTrackId).innerHTML = FirstEpisode.find("description").text();
           firstEpisodeUrl = FirstEpisode.find("enclosure").attr('url');
           document.getElementById("playEp"+jsonTrackId).innerHTML = "<audio controls style='width: 100%'><source src=" + firstEpisodeUrl + " type='audio/mpeg'></audio><br>";
-          document.getElementById("epList"+jsonTrackId).innerHTML = "<a href='#'>Episode List</a>";
-          displayEpisodes(episodes, jsonTrackId);
+            document.getElementById("epList"+jsonTrackId).innerHTML += "<strong><a href='results.html?result=" + jsonTrackId + "'>View Episodes</a></strong>";
+          // displayEpisodes(episodes, jsonTrackId);
         });
 }
 
 function displayEpisodes(eps, jsonTrackId) {
   console.log(eps);
-  for(var i = 1; i < 10; i++){
+  for(var i = 0; i < 50; i++){
 
     var title = $(eps[i]).find("title").text();
     var url = $(eps[i]).find("enclosure").attr('url');
+    var descript = $(eps[i]).find("description")["0"].textContent;
+    descript = descript.trim();
+    var dateRaw  = $(eps[i]).find("pubdate").textContent;
+    var pubdate = "";
 
-    document.getElementById("epListing"+jsonTrackId).innerHTML += "<p>" +title + "</p>" + "<audio controls style='width: 100%'><source src=" + url + " type='audio/mpeg'></audio><br>";
+    console.log(descript);
+    console.log(pubdate);
+
+    //TEST - clear date, need to fix this to correctly find the date of publish
+    //dateRaw.slice(0,16) -- slice for fixing date format
+
+    document.getElementById("epResultsDiv").innerHTML += "<p><strong>" + title + "</strong><span class='artist'> "+ pubdate + "</span></p><p>" + descript + "</p>" + "<audio controls style='width: 100%'><source src=" + url + " type='audio/mpeg'></audio><br><br>";
   }
 
-  document.getElementById("epListing"+jsonTrackId).innerHTML += "<strong>See More</strong>";
+
+}
+
+
+function showPodcastDeets(id) {
+  var searchTerm = id;
+  var url = 'https://itunes.apple.com/search?term=' + searchTerm + '&media=podcast&limit=10';
+  var json_obj = JSON.parse(getJSONPods(url));
+  var resultCount = json_obj.resultCount;
+  var jsonResults = json_obj.results[0];
+  var getXMLParameters = "https://cors-anywhere.herokuapp.com/" + jsonResults.feedUrl;
+  var jsonTrackId = id;
+
+  $.when($.get(getXMLParameters, function (data) {
+    })).then(function(data){
+      console.log(data);
+      var podcastDescript = $(data).find("channel > description");
+      var episodes = $(data).find("item");
+      var resultsDiv = document.getElementById("epResultsDiv");
+
+      document.getElementById("descriptionDeets").innerHTML = "<div class='row podResult'><div class='col-sm-3'><img src='" + jsonResults.artworkUrl600 + "' style='width: 100%;'></div> <div class='col-sm-9'><p class='podTitle' id='result" + jsonResults.trackId + "'>" + jsonResults.trackName + "</a><br><p class='artist'>" + jsonResults.artistName + "</p><p id='description" + jsonResults.trackId + "'>"+ podcastDescript["0"].textContent + "</p></div></div></div>";
+
+
+      var epListLink = document.getElementById("epList"+jsonTrackId);
+      //document.getElementById("epDescript"+jsonTrackId).innerHTML = FirstEpisode.find("description").text();
+      displayEpisodes(episodes, jsonTrackId);
+    });
+
 }
